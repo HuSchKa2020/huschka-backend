4d813713a0b5435a71bf7ccaf483ce0ce6bb375c
Wilhelm-kaden <70752464+Wilhelm-kaden@users.noreply.github.com>
2020-10-26 10:46:17 +0100

    Änderung

--- v1/getShoppingListbyUser.php
+++ v1/getShoppingListbyUser.php
@@ -5,26 +5,26 @@ require_once'../include/DbOperations.php';
 $response = array();
 session_start();
 if($_SERVER['REQUEST_METHOD']=='POST'){
-    
-  if(isset($_POST['ListenID']) ){
-      
+
+  if(isset($_POST['KundenID']) ){
+
       $db = new DbOperations();
-      
-      
-        $response = $db->getShoppingListbyUser($_POST['ListenID']);
-        
-      
+
+
+        $response = $db->getShoppingListbyUser($_POST['KundenID']);
+
+
   } else{
-      
+
     $response['error'] = true;
-    
+
     $response['message'] = "Required fields are missing";
-    
-    } 
+
+    }
 }else{
-    
+
   $response['error'] = true;
-  
+
   $response['message'] = "Invalid Request";
 }
 
