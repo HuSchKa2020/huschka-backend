6c473c4659f2757330f7363fe50a8aef19b67f0f
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-01-10 18:06:47 +0100

    senden der Mail via skript, mail muss noch eingerichtet werden

--- v1/registerUser.php
+++ v1/registerUser.php
@@ -25,18 +25,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 $vorname = $_POST['Vorname'];
                 $nachname = $_POST['Nachname'];
 
+                $vkey = md5(time().$nachname);
 
-                if($db->createUser($_POST['Email'], $_POST['Password'], $_POST['Vorname'], $_POST['Nachname'], $_POST['Adresse'])){
+                if($db->createUser($_POST['Email'], $_POST['Password'], $_POST['Vorname'], $_POST['Nachname'], $_POST['Adresse'], $vkey){
                     $user = $db->idAusgeben($_POST['Email']);                                                       //variable, die auf die function idAusgeben zugreift siehe Skript DbOperations
                     $response['error'] = false;
                     $response['message'] = "User registered successfully";
                     $response['UserID'] = $user['id'];
                 
-                    $key =  + $user['id'];
-                
                     $betreff = "Ihre HuSchKa Verifizierungsmail!" 
 
-                    $message = "Hallo '$vorname', bitte klicke <a href="'huschka.ddnss.de/huschka/verify.php'">hier</a> um dich zu verifizieren"
+                    $message = "Hallo '$vorname', bitte klicke <a href="'huschka.ddnss.de/huschka/verify.php?vkey=$vkey">hier</a> um dich zu verifizieren"
 
                     $headers = "From: The Sender Name <sender@huebner.de>\r\n";
                     $headers .= "Reply-To: reply@jan.de\r\n";
